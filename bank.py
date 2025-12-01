6b742eaeea90613041fc4d5a2507bc109239d16e
Hexolar <pixtrixtrick@gmail.com>
2025-12-01 11:09:24 +0200

    Python version

---
+++ bank.py
@@ -0,0 +1,17 @@
+class bankAccount():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > 0 and self.balance >= amount:
+            self.balance -= amount
+
+
+    def printInfo(self):
+        print(self.owner)
+        print(self.balance)
---
+++ main.py
@@ -0,0 +1,5 @@
+from bank import bankAccount
+
+Jack = bankAccount("JACK", 1000)
+Jack.deposit(500)
+Jack.print()
